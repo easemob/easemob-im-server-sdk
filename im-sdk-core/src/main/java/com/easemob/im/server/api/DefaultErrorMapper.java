205c1cd22399da5faa8c5887b1d744a3dd788c64
Harry Hao <harry-hao@outlook.com>
2021-03-17 00:23:08 +0800

    Replaced reflection with plain if else.

--- im-sdk-core/src/main/java/com/easemob/im/server/api/DefaultErrorMapper.java
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/DefaultErrorMapper.java
@@ -16,61 +16,40 @@ public class DefaultErrorMapper implements ErrorMapper {
 
     private static final Logger log = LoggerFactory.getLogger(DefaultErrorMapper.class);
 
-    private Map<HttpResponseStatus, Class<? extends EMException>> mappers;
-
-    public DefaultErrorMapper() {
-        this.mappers = new ConcurrentHashMap<>();
-        register(HttpResponseStatus.BAD_REQUEST, EMBadRequestException.class);
-        register(HttpResponseStatus.UNAUTHORIZED, EMUnauthorizedException.class);
-        register(HttpResponseStatus.FORBIDDEN, EMForbiddenException.class);
-        register(HttpResponseStatus.NOT_FOUND, EMNotFoundException.class);
-        register(HttpResponseStatus.METHOD_NOT_ALLOWED, EMMethodAllowedException.class);
-        register(HttpResponseStatus.NOT_ACCEPTABLE, EMNotAcceptableException.class);
-        register(HttpResponseStatus.UNSUPPORTED_MEDIA_TYPE, EMUnSupportedMediaTypeException.class);
-        register(HttpResponseStatus.TOO_MANY_REQUESTS, EMTooManyRequestsException.class);
-        register(HttpResponseStatus.INTERNAL_SERVER_ERROR, EMInternalServerErrorException.class);
-        register(HttpResponseStatus.BAD_GATEWAY, EMBadGatewayException.class);
-        register(HttpResponseStatus.SERVICE_UNAVAILABLE, EMServiceUnavailableException.class);
-        register(HttpResponseStatus.GATEWAY_TIMEOUT, EMGatewayTimeoutException.class);
-
-    }
-
-    public void register(HttpResponseStatus status, Class<? extends EMException> exception) {
-        this.mappers.put(status, exception);
-        log.debug("http error mapper registered for status code {}, map to exception {}", status.code(), exception);
-    }
-
-    public void unregister(HttpResponseStatus status) {
-        this.mappers.remove(status);
-        log.debug("http error mapper unregistered for status code {}", status.code());
-    }
-
-    @SuppressWarnings("unchecked")
     public Mono<HttpClientResponse> apply(HttpClientResponse response) {
-        int code = response.status().code();
-        Class<? extends EMException> errorClass = this.mappers.get(response.status());
-
-        if (errorClass == null) {
+        if (response.status().code() < 400) {
             return Mono.just(response);
         }
+        return Mono.error(toException(response));
+    }
 
-        String reason = String.format("%s %s return %d %s", response.method().toString(), response.uri(),
-                response.status().code(), response.status().reasonPhrase());
-        Constructor<?>[] ctors = errorClass.getConstructors();
-        for (int i = 0; i < ctors.length; i++) {
-            if (ctors[i].getParameterCount() == 1 && ctors[i].getParameterTypes()[0] == String.class) {
-                EMException error;
-                try {
-                    error = (EMException) ctors[i].newInstance(reason);
-                } catch (Exception e) {
-                    return Mono.error(new EMUnknownException(reason));
-                }
-                return Mono.error(error);
-            }
+    private EMException toException(HttpClientResponse response) {
+        String reason = String.format("%s %s -> %d %s", response.method().toString(), response.uri(), response.status().code(), response.status().reasonPhrase());
+        HttpResponseStatus status = response.status();
+        if (HttpResponseStatus.BAD_REQUEST.equals(status)
+                || HttpResponseStatus.METHOD_NOT_ALLOWED.equals(status)
+                || HttpResponseStatus.NOT_ACCEPTABLE.equals(status)
+                || HttpResponseStatus.UNSUPPORTED_MEDIA_TYPE.equals(status)) {
+            return new EMBadRequestException(reason);
+        } else if (HttpResponseStatus.UNAUTHORIZED.equals(status)) {
+            return new EMUnauthorizedException(reason);
+        } else if (HttpResponseStatus.PAYMENT_REQUIRED.equals(status)
+                || HttpResponseStatus.FORBIDDEN.equals(status)) {
+            return new EMForbiddenException(reason);
+        } else if (HttpResponseStatus.NOT_FOUND.equals(status)) {
+            return new EMNotFoundException(reason);
+        } else if (HttpResponseStatus.TOO_MANY_REQUESTS.equals(status)) {
+            return new EMTooManyRequestsException(reason);
+        } else if (HttpResponseStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+            return new EMInternalServerErrorException(reason);
+        } else if (HttpResponseStatus.BAD_GATEWAY.equals(status)) {
+            return new EMBadGatewayException(reason);
+        } else if (HttpResponseStatus.SERVICE_UNAVAILABLE.equals(status)) {
+            return new EMServiceUnavailableException(reason);
+        } else if (HttpResponseStatus.GATEWAY_TIMEOUT.equals(status)) {
+            return new EMGatewayTimeoutException(reason);
         }
-        return Mono.error(new EMUnknownException(reason));
-
-
+        return new EMUnknownException(reason);
     }
 
 }
