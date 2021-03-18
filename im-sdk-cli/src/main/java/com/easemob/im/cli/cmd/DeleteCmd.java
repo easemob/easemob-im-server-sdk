cefc27008a78ee6dce99c09945ed5b306b19dfb4
Yubing <yubinnng@gmail.com>
2021-03-18 16:33:16 +0800

    Add force use logout CLI

--- im-sdk-cli/src/main/java/com/easemob/im/cli/cmd/DeleteCmd.java
+++ im-sdk-cli/src/main/java/com/easemob/im/cli/cmd/DeleteCmd.java
@@ -5,11 +5,13 @@ import com.easemob.im.server.EMService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+
+import static picocli.CommandLine.Option;
+import static picocli.CommandLine.Parameters;
 
 @Component
 @Command(name = "delete", description = "Delete a resource.")
@@ -19,12 +21,12 @@ public class DeleteCmd {
     private EMService service;
 
     @Command(name = "block", description = "Unblock user from resource.")
-    public void block(@CommandLine.Parameters(index = "0", description = "user to unblock") String username,
-                      @CommandLine.Option(names = {"--msg-to-user"}, description = "unblock user send message to this user") String msgToUsername,
-                      @CommandLine.Option(names = {"--msg-to-group"}, description = "unblock user send message to this group") String msgToGroupId,
-                      @CommandLine.Option(names = {"--msg-to-room"}, description = "unblock user send message to this room") String msgToRoomId,
-                      @CommandLine.Option(names = {"--login"}, description = "unblock user to login") boolean login,
-                      @CommandLine.Option(names = {"--join-group"}, description = "unblock user to join group") String groupId) {
+    public void block(@Parameters(index = "0", description = "user to unblock") String username,
+                      @Option(names = {"--msg-to-user"}, description = "unblock user send message to this user") String msgToUsername,
+                      @Option(names = {"--msg-to-group"}, description = "unblock user send message to this group") String msgToGroupId,
+                      @Option(names = {"--msg-to-room"}, description = "unblock user send message to this room") String msgToRoomId,
+                      @Option(names = {"--login"}, description = "unblock user to login") boolean login,
+                      @Option(names = {"--join-group"}, description = "unblock user to join group") String groupId) {
         if (StringUtils.hasText(msgToUsername)) {
             this.service.block().unblockUserSendMsgToUser(username, msgToUsername)
                     .doOnSuccess(ignore -> System.out.println("done"))
@@ -56,28 +58,28 @@ public class DeleteCmd {
     }
 
     @Command(name = "user", description = "Delete a user.", mixinStandardHelpOptions = true)
-    public void user(@CommandLine.Parameters(index = "0", paramLabel = "username", description = "user to unblock", defaultValue = "") String username,
-                       @CommandLine.Option(names = {"--all"}, description = "delete all users") boolean all) {
+    public void user(@Parameters(index = "0", paramLabel = "username", description = "user to unblock", defaultValue = "") String username,
+                     @Option(names = {"--all"}, description = "delete all users") boolean all) {
         if (all) {
             this.service.user().deleteAll()
                     .doOnNext(user -> System.out.println("user " + user.getUsername() + " deleted"))
                     .doOnError(err -> System.out.println("error: " + err.getMessage()))
                     .onErrorResume(EMException.class, ignore -> Mono.empty())
                     .blockLast();
-        } else if(StringUtils.hasText(username)){
+        } else if (StringUtils.hasText(username)) {
             this.service.user().delete(username)
                     .doOnSuccess(user -> System.out.println("user " + user.getUsername() + " deleted"))
                     .doOnError(err -> System.out.println("error: " + err.getMessage()))
                     .onErrorResume(EMException.class, ignore -> Mono.empty())
                     .block();
         } else {
-            System.out.println("must specify one of username or --all");
+            System.out.println("must specify one username or use --all");
         }
     }
 
     @Command(name = "contact", description = "Remove a contact from the user.")
-    public void contact(@CommandLine.Parameters(index = "0", description = "the user's username") String user,
-                              @CommandLine.Parameters(index = "1", description = "the contact's username") String contact) {
+    public void contact(@Parameters(index = "0", description = "the user's username") String user,
+                        @Parameters(index = "1", description = "the contact's username") String contact) {
         this.service.contact().remove(user, contact)
                 .doOnSuccess(ignored -> System.out.println("done"))
                 .doOnError(err -> System.out.println("error: " + err.getMessage()))
@@ -87,11 +89,32 @@ public class DeleteCmd {
 
     @Command(name = "group", description = "Delete a group.\n" +
             "Messages will be destroyed with the group, while the chat history is reserved.")
-    public void group(@CommandLine.Parameters(index = "0", description = "the group's id") String groupId) {
+    public void group(@Parameters(index = "0", description = "the group's id") String groupId) {
         this.service.group().destroyGroup(groupId)
                 .doOnSuccess(ignored -> System.out.println("done"))
                 .doOnError(err -> System.out.println("error: " + err.getMessage()))
                 .onErrorResume(EMException.class, ignore -> Mono.empty())
                 .block();
+    }
+
+    @Command(name = "session", description = "Force user logout.")
+    public void session(@Parameters(index = "0", description = "the username") String username,
+                        @Parameters(index = "1", description = "the device name", defaultValue = "") String deviceName,
+                        @Option(names = "--all", description = "force logout all devices") boolean all) {
+        if (all) {
+            this.service.user().forceLogoutAllDevices(username)
+                    .doOnSuccess(ignored -> System.out.println("done"))
+                    .doOnError(err -> System.out.println("error: " + err.getMessage()))
+                    .onErrorResume(EMException.class, ignore -> Mono.empty())
+                    .block();
+        } else if (StringUtils.hasText(deviceName)) {
+            this.service.user().forceLogoutOneDevice(username, deviceName)
+                    .doOnSuccess(ignored -> System.out.println("done"))
+                    .doOnError(err -> System.out.println("error: " + err.getMessage()))
+                    .onErrorResume(EMException.class, ignore -> Mono.empty())
+                    .block();
+        } else {
+            System.out.println("must specify one device or use --all");
+        }
     }
 }
