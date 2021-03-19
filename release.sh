367002ec364b05b43c05b499559e5a065c484dfa
Harry Hao <harry-hao@outlook.com>
2021-03-19 14:43:20 +0800

    Prepare for release.

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# usage: ./release.sh 当前Release版本 下一个开发版本
+
+RELEASE=$1
+SNAPSHOT=$2
+
+./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
+git add .
+git commit --message "v$RELEASE Release"
+
+# Tag the release
+git tag -s v$RELEASE -m "v$RELEASE"
+
+# Bump up the version in pom.xml to the next snapshot
+./mvnw versions:set -DnewVersion="$SNAPSHOT-SNAPSHOT" -DgenerateBackupPoms=false
+git add .
+git commit --message "v$SNAPSHOT Development"
