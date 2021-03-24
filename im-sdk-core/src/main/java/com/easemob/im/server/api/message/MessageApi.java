55e8b293786af530e3d1a9095a705de74570352b
Yubing <yubinnng@gmail.com>
2021-03-24 18:09:39 +0800

    Unify message cli, integrate `get history cli` to `get message --history`.

--- im-sdk-core/src/main/java/com/easemob/im/server/api/message/MessageApi.java
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/message/MessageApi.java
@@ -34,12 +34,15 @@ public class MessageApi {
 
     private MessageHistory messageHistory;
 
+    private MessageStatus messageStatus;
+
     public MessageApi(Context context) {
         EMProperties properties = context.getProperties();
         this.context = context;
         this.missed = new MessageMissed(context);
         this.sendMessage = new SendMessage(context);
         this.messageHistory = new MessageHistory(context, properties.getServerTimezone());
+        this.messageStatus = new MessageStatus(context);
     }
 
     /**
@@ -47,14 +50,21 @@ public class MessageApi {
      *
      * @param username 用户名
      * @return 每个对话的离线消息数
-     * @see com.easemob.im.server.api.message.missed.MissedMessageCount
+     * @see <a href="http://docs-im.easemob.com/im/server/ready/user#%E8%8E%B7%E5%8F%96%E7%94%A8%E6%88%B7%E7%A6%BB%E7%BA%BF%E6%B6%88%E6%81%AF%E6%95%B0">获取用户离线消息数</a>
      */
     public Flux<MissedMessageCount> countMissedMessages(String username) {
         return this.missed.count(username);
     }
 
+    /**
+     * 查询某条离线消息的状态，如是否已经传达。
+     * @param messageId 要查看对消息ID
+     * @param toUser 消息接收方的用户名
+     * @return 是否送达或错误
+     * @see <a href="http://docs-im.easemob.com/im/server/ready/user#%E8%8E%B7%E5%8F%96%E6%9F%90%E6%9D%A1%E7%A6%BB%E7%BA%BF%E6%B6%88%E6%81%AF%E7%8A%B6%E6%80%81">获取某条离线消息状态</a>
+     */
     public Mono<Boolean> isMessageDeliveredToUser(String messageId, String toUser) {
-        return MessageStatus.isMessageDeliveredToUser(this.context, messageId, toUser);
+        return this.messageStatus.isMessageDeliveredToUser(messageId, toUser);
     }
 
     /**
@@ -72,7 +82,7 @@ public class MessageApi {
      * }</pre>
      *
      * @return 发送消息的构造器
-     * @see com.easemob.im.server.api.message.send.SendMessage
+     * @see <a href="http://docs-im.easemob.com/im/server/basics/messages">发送消息</a>
      */
     public SendMessage send() {
         return this.sendMessage;
@@ -87,8 +97,7 @@ public class MessageApi {
      * @param message 要发送的消息
      * @param extensions 要发送的扩展，可以为空
      * @return 发消息响应或错误
-     * @see com.easemob.im.server.model.EMMessage
-     * @see com.easemob.im.server.model.EMKeyValue
+     * @see <a href="http://docs-im.easemob.com/im/server/basics/messages">发送消息</a>
      */
     public Mono<EMSentMessages> send(String from, String toType, Set<String> tos, EMMessage message, Set<EMKeyValue> extensions) {
         return this.sendMessage.send(from, toType, tos, message, extensions);
@@ -99,6 +108,7 @@ public class MessageApi {
      * 历史文件是每小时一个文件，比如指定12:10，则返回12点的历史文件。
      * @param instant 时间点
      * @return 下载地址或错误
+     * @see <a href="http://docs-im.easemob.com/im/server/basics/chatrecord#%E8%8E%B7%E5%8F%96%E5%8E%86%E5%8F%B2%E6%B6%88%E6%81%AF%E6%96%87%E4%BB%B6">获取历史消息文件</a>
      */
     public Mono<String> getHistoryAsUri(Instant instant) {
         return this.messageHistory.toUri(instant);
@@ -110,7 +120,8 @@ public class MessageApi {
      * @param instant 时间点
      * @param dir 下载目录
      * @param filename 文件名，如果为空，则默认为YYYYMMDD.gz
-     * @return A {@code Mono} of the path of downloaded file
+     * @return 下载文件的路径或错误
+     * @see <a href="http://docs-im.easemob.com/im/server/basics/chatrecord#%E8%8E%B7%E5%8F%96%E5%8E%86%E5%8F%B2%E6%B6%88%E6%81%AF%E6%96%87%E4%BB%B6">获取历史消息文件</a>
      */
     public Mono<Path> getHistoryAsLocalFile(Instant instant, Path dir, String filename) {
         return this.messageHistory.toLocalFile(instant, dir, filename);
