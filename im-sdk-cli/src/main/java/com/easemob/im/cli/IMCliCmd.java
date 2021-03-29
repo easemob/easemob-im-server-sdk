0c992ed30da08937047d9c4f6258177c284c0b72
Yubing <yubinnng@gmail.com>
2021-03-29 14:26:35 +0800

    Add `-V` and `--version` option.

--- im-sdk-cli/src/main/java/com/easemob/im/cli/IMCliCmd.java
+++ im-sdk-cli/src/main/java/com/easemob/im/cli/IMCliCmd.java
@@ -6,15 +6,20 @@ import com.easemob.im.cli.cmd.CreateCmd;
 import com.easemob.im.cli.cmd.DeleteCmd;
 import com.easemob.im.cli.cmd.GetCmd;
 import com.easemob.im.cli.cmd.UpdateCmd;
+import com.easemob.im.server.EMVersion;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+
+import java.util.Arrays;
 
 @Component
 @Command(name = "im",
         mixinStandardHelpOptions = true,
+        versionProvider = Version.class,
         subcommands = {
                 CreateCmd.class,
                 GetCmd.class,
@@ -32,5 +37,15 @@ public class IMCliCmd implements InitializingBean {
             Logger root = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
             root.setLevel(Level.DEBUG);
         }
+    }
+}
+
+class Version implements CommandLine.IVersionProvider {
+
+    @Override
+    public String[] getVersion() {
+        String[] versions = new String[1];
+        versions[0] = EMVersion.getVersion();
+        return versions;
     }
 }
