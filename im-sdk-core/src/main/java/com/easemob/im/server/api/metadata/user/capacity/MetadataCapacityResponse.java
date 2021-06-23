c36e07cf186ca530a942b7d36b720c78fa6168a3
Ken Wang <kenuiuc@gmail.com>
2021-06-23 17:33:54 +0800

    remove some notNull enforcement in metadata api

--- im-sdk-core/src/main/java/com/easemob/im/server/api/metadata/user/capacity/MetadataCapacityResponse.java
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/metadata/user/capacity/MetadataCapacityResponse.java
@@ -1,23 +1,18 @@
 package com.easemob.im.server.api.metadata.user.capacity;
 
-import com.easemob.im.server.exception.EMInvalidArgumentException;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MetadataCapacityResponse {
-    // capacity would never be null so just use the primitive type here
     @JsonProperty("data")
-    private long capacity;
+    private Long capacity;
 
     @JsonCreator
     public MetadataCapacityResponse(@JsonProperty("data") Long capacity) {
-        if (capacity == null) {
-            throw new EMInvalidArgumentException("metadata capacity value is null");
-        }
         this.capacity = capacity;
     }
 
-    public long getCapacity() {
+    public Long getCapacity() {
         return capacity;
     }
 }
--- im-sdk-core/src/test/java/com/easemob/im/server/api/metadata/user/delete/MetadataDeleteTest.java
+++ im-sdk-core/src/test/java/com/easemob/im/server/api/metadata/user/delete/MetadataDeleteTest.java
@@ -1,10 +1,8 @@
 package com.easemob.im.server.api.metadata.user.delete;
 
 import com.easemob.im.server.api.AbstractApiTest;
-import com.easemob.im.server.exception.EMInvalidArgumentException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.base.Throwables;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -16,7 +14,6 @@ class MetadataDeleteTest extends AbstractApiTest {
 
     public MetadataDeleteTest() {
         this.server.addHandler("DELETE /easemob/demo/metadata/user/bob", this::handleMetadataDeleteSuccess);
-        this.server.addHandler("DELETE /easemob/demo/metadata/user/cat", this::handleMetadataDeleteNull);
     }
 
     @Test
@@ -25,20 +22,9 @@ class MetadataDeleteTest extends AbstractApiTest {
         assertTrue(deletionSuccess);
     }
 
-    @Test
-    public void testMetadataDeleteFromUserNull() {
-        Throwable throwable = assertThrows(Throwable.class, () -> this.metadataDelete.fromUser("cat").block(Duration.ofSeconds(3)));
-        assertEquals(EMInvalidArgumentException.class, Throwables.getRootCause(throwable).getClass());
-    }
-
     public JsonNode handleMetadataDeleteSuccess(JsonNode req) {
         ObjectNode jsonRsp = this.objectMapper.createObjectNode();
         jsonRsp.put("data", true);
-        return jsonRsp;
-    }
-
-    public JsonNode handleMetadataDeleteNull(JsonNode req) {
-        ObjectNode jsonRsp = this.objectMapper.createObjectNode();
         return jsonRsp;
     }
 }
