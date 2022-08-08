0911942aa30e642a77298e44cc0583c40abe41f1
ZYongkang <zhangyk_01@163.com>
2022-08-08 16:06:33 +0800

    add get user token with inherit

---
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/token/allocate/InheritTokenRequest.java
@@ -0,0 +1,45 @@
+package com.easemob.im.server.api.token.allocate;
+
+import com.easemob.im.server.exception.EMInvalidArgumentException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.logging.log4j.util.Strings;
+
+public class InheritTokenRequest implements TokenRequest {
+
+    @SuppressWarnings("java:S1170")
+    @JsonProperty("grant_type")
+    private final String grantType = "inherit";
+
+    @JsonProperty("username")
+    private final String username;
+
+    @JsonProperty("autoCreateUser")
+    private final Boolean autoCreateUser;
+
+    public InheritTokenRequest(String username, Boolean autoCreateUser) {
+        this.username = username;
+        this.autoCreateUser = autoCreateUser;
+    }
+
+    @JsonCreator
+    public static InheritTokenRequest of(@JsonProperty("username") String username,
+            @JsonProperty("autoCreateUser") Boolean autoCreateUser) {
+        if (Strings.isBlank(username)) {
+            throw new EMInvalidArgumentException("username must not be null or blank");
+        }
+        return new InheritTokenRequest(username, autoCreateUser);
+    }
+
+    public String getGrantType() {
+        return grantType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Boolean getAutoCreateUser() {
+        return autoCreateUser;
+    }
+}
