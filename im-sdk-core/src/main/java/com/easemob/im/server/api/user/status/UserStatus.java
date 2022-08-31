08f7b2717fb625c77e52217f4bf5ed6095f46f08
skyfour <zhangjiap@163.com>
2022-08-31 17:36:21 +0800

    fix user api not intercept request when http status is error issue

--- im-sdk-core/src/main/java/com/easemob/im/server/api/user/status/UserStatus.java
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/user/status/UserStatus.java
@@ -1,8 +1,14 @@
 package com.easemob.im.server.api.user.status;
 
+import com.easemob.im.server.EMException;
 import com.easemob.im.server.api.Context;
+import com.easemob.im.server.api.DefaultErrorMapper;
+import com.easemob.im.server.api.ErrorMapper;
+import com.easemob.im.server.api.user.list.UserListResponse;
+import com.easemob.im.server.exception.EMUnknownException;
 import com.easemob.im.server.model.EMUserStatus;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClientResponse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,14 +26,25 @@ public class UserStatus {
         return context.getHttpClient()
                 .flatMap(httpClient -> httpClient.get()
                         .uri(String.format("/users/%s/status", username))
-                        .responseSingle(
-                                (rsp, buf) -> {
-                                    this.context.getErrorMapper().statusCode(rsp);
-                                    return buf;
-                                })
-                        .doOnNext(buf -> this.context.getErrorMapper().checkError(buf)))
-                .map(buf -> context.getCodec().decode(buf, UserStatusResponse.class))
-                .map(rsp -> rsp.isUserOnline(username));
+                        .responseSingle((rsp, buf) -> Mono.zip(Mono.just(rsp), buf))
+                        .flatMap(tuple2 -> {
+                            HttpClientResponse clientResponse = tuple2.getT1();
+
+                            return Mono.defer(() -> {
+                                ErrorMapper mapper = new DefaultErrorMapper();
+                                mapper.statusCode(clientResponse);
+                                mapper.checkError(tuple2.getT2());
+                                return Mono.just(tuple2.getT2());
+                            }).onErrorResume(e -> {
+                                if (e instanceof EMException) {
+                                    return Mono.error(e);
+                                }
+                                return Mono.error(new EMUnknownException(e.getMessage()));
+                            }).flatMap(byteBuf -> {
+                                UserStatusResponse userStatusResponse = this.context.getCodec().decode(byteBuf, UserStatusResponse.class);
+                                return Mono.just(userStatusResponse.isUserOnline(username));
+                            });
+                        }));
     }
 
     public Mono<List<EMUserStatus>> isUsersOnline(List<String> usernames) {
@@ -36,13 +53,24 @@ public class UserStatus {
                         .uri("/users/batch/status")
                         .send(Mono.create(sink -> sink.success(this.context.getCodec()
                                 .encode(new UserStatusBatchQueryRequest(usernames)))))
-                        .responseSingle(
-                                (rsp, buf) -> {
-                                    this.context.getErrorMapper().statusCode(rsp);
-                                    return buf;
-                                })
-                        .doOnNext(buf -> this.context.getErrorMapper().checkError(buf)))
-                .map(buf -> context.getCodec().decode(buf, UserStatusBatchQueryResponse.class))
-                .map(rsp -> rsp.getUsersOnline());
+                        .responseSingle((rsp, buf) -> Mono.zip(Mono.just(rsp), buf))
+                        .flatMap(tuple2 -> {
+                            HttpClientResponse clientResponse = tuple2.getT1();
+
+                            return Mono.defer(() -> {
+                                ErrorMapper mapper = new DefaultErrorMapper();
+                                mapper.statusCode(clientResponse);
+                                mapper.checkError(tuple2.getT2());
+                                return Mono.just(tuple2.getT2());
+                            }).onErrorResume(e -> {
+                                if (e instanceof EMException) {
+                                    return Mono.error(e);
+                                }
+                                return Mono.error(new EMUnknownException(e.getMessage()));
+                            }).flatMap(byteBuf -> {
+                                UserStatusBatchQueryResponse userStatusBatchQueryResponse = this.context.getCodec().decode(byteBuf, UserStatusBatchQueryResponse.class);
+                                return Mono.just(userStatusBatchQueryResponse.getUsersOnline());
+                            });
+                        }));
     }
 }
--- im-sdk-core/src/main/java/com/easemob/im/server/api/user/unregister/DeleteUser.java
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/user/unregister/DeleteUser.java
@@ -1,10 +1,15 @@
 package com.easemob.im.server.api.user.unregister;
 
+import com.easemob.im.server.EMException;
 import com.easemob.im.server.api.Context;
+import com.easemob.im.server.api.DefaultErrorMapper;
+import com.easemob.im.server.api.ErrorMapper;
+import com.easemob.im.server.api.user.list.UserListResponse;
 import com.easemob.im.server.exception.EMUnknownException;
 import io.netty.handler.codec.http.QueryStringEncoder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClientResponse;
 
 public class DeleteUser {
 
@@ -18,20 +23,22 @@ public class DeleteUser {
         return this.context.getHttpClient()
                 .flatMap(httpClient -> httpClient.delete()
                         .uri(String.format("/users/%s", username))
-                        .responseSingle(
-                                (rsp, buf) -> {
-                                    this.context.getErrorMapper().statusCode(rsp);
-                                    return buf;
-                                })
-                        .doOnNext(buf -> this.context.getErrorMapper().checkError(buf)))
-                .map(b -> this.context.getCodec().decode(b, UserUnregisterResponse.class))
-                .handle((rsp, sink) -> {
-                    if (rsp.getError() != null) {
-                        sink.error(new EMUnknownException(rsp.getError()));
-                        return;
-                    }
-                    sink.complete();
-                });
+                        .responseSingle((rsp, buf) -> Mono.zip(Mono.just(rsp), buf))
+                        .flatMap(tuple2 -> {
+                            HttpClientResponse clientResponse = tuple2.getT1();
+
+                            return Mono.defer(() -> {
+                                ErrorMapper mapper = new DefaultErrorMapper();
+                                mapper.statusCode(clientResponse);
+                                mapper.checkError(tuple2.getT2());
+                                return Mono.just(tuple2.getT2());
+                            }).onErrorResume(e -> {
+                                if (e instanceof EMException) {
+                                    return Mono.error(e);
+                                }
+                                return Mono.error(new EMUnknownException(e.getMessage()));
+                            }).then();
+                        }));
     }
 
     public Flux<String> all(int limit) {
@@ -53,12 +60,24 @@ public class DeleteUser {
         return this.context.getHttpClient()
                 .flatMap(httpClient -> httpClient.delete()
                         .uri(uirString)
-                        .responseSingle(
-                                (rsp, buf) -> {
-                                    this.context.getErrorMapper().statusCode(rsp);
-                                    return buf;
-                                })
-                        .doOnNext(buf -> this.context.getErrorMapper().checkError(buf)))
-                .map(b -> this.context.getCodec().decode(b, UserUnregisterResponse.class));
+                        .responseSingle((rsp, buf) -> Mono.zip(Mono.just(rsp), buf))
+                        .flatMap(tuple2 -> {
+                            HttpClientResponse clientResponse = tuple2.getT1();
+
+                            return Mono.defer(() -> {
+                                ErrorMapper mapper = new DefaultErrorMapper();
+                                mapper.statusCode(clientResponse);
+                                mapper.checkError(tuple2.getT2());
+                                return Mono.just(tuple2.getT2());
+                            }).onErrorResume(e -> {
+                                if (e instanceof EMException) {
+                                    return Mono.error(e);
+                                }
+                                return Mono.error(new EMUnknownException(e.getMessage()));
+                            }).flatMap(byteBuf -> {
+                                UserUnregisterResponse userUnregisterResponse = this.context.getCodec().decode(byteBuf, UserUnregisterResponse.class);
+                                return Mono.just(userUnregisterResponse);
+                            });
+                        }));
     }
 }
