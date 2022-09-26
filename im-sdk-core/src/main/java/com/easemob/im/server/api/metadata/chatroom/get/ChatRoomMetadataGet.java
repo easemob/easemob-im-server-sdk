89089ef0ae75a0b627782cdba17e3e4077ba7cf2
newBlashOfKing <18401432360@163.com>
2022-09-26 11:41:03 +0800

    update metadata api response code

--- im-sdk-core/src/main/java/com/easemob/im/server/api/metadata/chatroom/get/ChatRoomMetadataGet.java
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/metadata/chatroom/get/ChatRoomMetadataGet.java
@@ -1,8 +1,13 @@
 package com.easemob.im.server.api.metadata.chatroom.get;
 
+import com.easemob.im.server.EMException;
 import com.easemob.im.server.api.Context;
+import com.easemob.im.server.api.DefaultErrorMapper;
+import com.easemob.im.server.api.ErrorMapper;
 import com.easemob.im.server.api.metadata.chatroom.ChatRoomMetadataRequest;
+import com.easemob.im.server.exception.EMUnknownException;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClientResponse;
 
 import java.util.List;
 
@@ -22,14 +27,28 @@ public class ChatRoomMetadataGet {
                         .uri(String.format("/metadata/chatroom/%s", chatroomId))
                         .send(Mono.create(sink -> sink.success(this.context.getCodec()
                                 .encode(new ChatRoomMetadataRequest(keys)))))
-                        .responseSingle(
-                                (rsp, buf) -> {
-                                    this.context.getErrorMapper().statusCode(rsp);
-                                    return buf;
-                                })
-                        .doOnNext(buf -> this.context.getErrorMapper().checkError(buf)))
-                .map(buf -> this.context.getCodec()
-                        .decode(buf, ChatRoomMetadataGetResponse.class));
+                        .responseSingle((rsp, buf) -> Mono.zip(Mono.just(rsp), buf))
+                        .flatMap(tuple2 -> {
+                            HttpClientResponse clientResponse = tuple2.getT1();
+
+                            return Mono.defer(() -> {
+                                ErrorMapper mapper = new DefaultErrorMapper();
+                                mapper.statusCode(clientResponse);
+                                mapper.checkError(tuple2.getT2());
+                                return Mono.just(tuple2.getT2());
+                            }).onErrorResume(e -> {
+                                if (e instanceof EMException) {
+                                    return Mono.error(e);
+                                }
+                                return Mono.error(new EMUnknownException(
+                                        String.format("chatroomId:%s", chatroomId)));
+                            }).flatMap(byteBuf -> {
+                                ChatRoomMetadataGetResponse
+                                        chatRoomMetadataGetResponse = this.context.getCodec()
+                                        .decode(byteBuf, ChatRoomMetadataGetResponse.class);
+                                return Mono.just(chatRoomMetadataGetResponse);
+                            });
+                        }));
     }
 
     public Mono<ChatRoomMetadataGetResponse> listChatRoomMetadataAll(String chatroomId) {
@@ -37,13 +56,27 @@ public class ChatRoomMetadataGet {
                 .flatMap(httpClient -> httpClient
                         .headers(header -> header.add("Content-Type", "application/json")).post()
                         .uri(String.format("/metadata/chatroom/%s", chatroomId))
-                        .responseSingle(
-                                (rsp, buf) -> {
-                                    this.context.getErrorMapper().statusCode(rsp);
-                                    return buf;
-                                })
-                        .doOnNext(buf -> this.context.getErrorMapper().checkError(buf)))
-                .map(buf -> this.context.getCodec()
-                        .decode(buf, ChatRoomMetadataGetResponse.class));
+                        .responseSingle((rsp, buf) -> Mono.zip(Mono.just(rsp), buf))
+                        .flatMap(tuple2 -> {
+                            HttpClientResponse clientResponse = tuple2.getT1();
+
+                            return Mono.defer(() -> {
+                                ErrorMapper mapper = new DefaultErrorMapper();
+                                mapper.statusCode(clientResponse);
+                                mapper.checkError(tuple2.getT2());
+                                return Mono.just(tuple2.getT2());
+                            }).onErrorResume(e -> {
+                                if (e instanceof EMException) {
+                                    return Mono.error(e);
+                                }
+                                return Mono.error(new EMUnknownException(
+                                        String.format("chatroomId:%s", chatroomId)));
+                            }).flatMap(byteBuf -> {
+                                ChatRoomMetadataGetResponse
+                                        chatRoomMetadataGetResponse = this.context.getCodec()
+                                        .decode(byteBuf, ChatRoomMetadataGetResponse.class);
+                                return Mono.just(chatRoomMetadataGetResponse);
+                            });
+                        }));
     }
 }
--- im-sdk-core/src/main/java/com/easemob/im/server/api/metadata/chatroom/set/ChatRoomMetadataSet.java
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/metadata/chatroom/set/ChatRoomMetadataSet.java
@@ -1,9 +1,15 @@
 package com.easemob.im.server.api.metadata.chatroom.set;
 
+import com.easemob.im.server.EMException;
 import com.easemob.im.server.api.Context;
+import com.easemob.im.server.api.DefaultErrorMapper;
+import com.easemob.im.server.api.ErrorMapper;
 import com.easemob.im.server.api.metadata.chatroom.AutoDelete;
 import com.easemob.im.server.api.metadata.chatroom.ChatRoomMetadataRequest;
+import com.easemob.im.server.api.user.get.UserGetResponse;
+import com.easemob.im.server.exception.EMUnknownException;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClientResponse;
 
 import java.util.Map;
 
@@ -24,13 +30,25 @@ public class ChatRoomMetadataSet {
                         .uri(String.format("/metadata/chatroom/%s/user/%s", chatroomId, operator))
                         .send(Mono.create(sink -> sink.success(this.context.getCodec()
                                 .encode(new ChatRoomMetadataRequest(metadata, autoDelete)))))
-                        .responseSingle(
-                                (rsp, buf) -> {
-                                    this.context.getErrorMapper().statusCode(rsp);
-                                    return buf;
-                                })
-                        .doOnNext(buf -> this.context.getErrorMapper().checkError(buf)))
-                .map(buf -> this.context.getCodec().decode(buf, ChatRoomMetadataSetResponse.class));
+                .responseSingle((rsp, buf) -> Mono.zip(Mono.just(rsp), buf))
+                .flatMap(tuple2 -> {
+                    HttpClientResponse clientResponse = tuple2.getT1();
+
+                    return Mono.defer(() -> {
+                        ErrorMapper mapper = new DefaultErrorMapper();
+                        mapper.statusCode(clientResponse);
+                        mapper.checkError(tuple2.getT2());
+                        return Mono.just(tuple2.getT2());
+                    }).onErrorResume(e -> {
+                        if (e instanceof EMException) {
+                            return Mono.error(e);
+                        }
+                        return Mono.error(new EMUnknownException(String.format("chatroomId:%s", chatroomId)));
+                    }).flatMap(byteBuf -> {
+                        ChatRoomMetadataSetResponse chatRoomMetadataSetResponse = this.context.getCodec().decode(byteBuf, ChatRoomMetadataSetResponse.class);
+                        return Mono.just(chatRoomMetadataSetResponse);
+                    });
+                }));
     }
 
     public Mono<ChatRoomMetadataSetResponse> toChatRoomForced(String operator,String chatroomId,
@@ -43,12 +61,24 @@ public class ChatRoomMetadataSet {
                                 operator))
                         .send(Mono.create(sink -> sink.success(this.context.getCodec()
                                 .encode(new ChatRoomMetadataRequest(metadata, autoDelete)))))
-                        .responseSingle(
-                                (rsp, buf) -> {
-                                    this.context.getErrorMapper().statusCode(rsp);
-                                    return buf;
-                                })
-                        .doOnNext(buf -> this.context.getErrorMapper().checkError(buf)))
-                .map(buf -> this.context.getCodec().decode(buf, ChatRoomMetadataSetResponse.class));
+                        .responseSingle((rsp, buf) -> Mono.zip(Mono.just(rsp), buf))
+                        .flatMap(tuple2 -> {
+                            HttpClientResponse clientResponse = tuple2.getT1();
+
+                            return Mono.defer(() -> {
+                                ErrorMapper mapper = new DefaultErrorMapper();
+                                mapper.statusCode(clientResponse);
+                                mapper.checkError(tuple2.getT2());
+                                return Mono.just(tuple2.getT2());
+                            }).onErrorResume(e -> {
+                                if (e instanceof EMException) {
+                                    return Mono.error(e);
+                                }
+                                return Mono.error(new EMUnknownException(String.format("chatroomId:%s", chatroomId)));
+                            }).flatMap(byteBuf -> {
+                                ChatRoomMetadataSetResponse chatRoomMetadataSetResponse = this.context.getCodec().decode(byteBuf, ChatRoomMetadataSetResponse.class);
+                                return Mono.just(chatRoomMetadataSetResponse);
+                            });
+                        }));
     }
 }
