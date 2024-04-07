df0e06ad4531576af28c0df5bafd92b7efd4a1f5
傅东海 <296322762@qq.com>
2024-04-07 16:34:05 +0800

    update

--- src/main/java/com/easemob/im/ApiClient.java
+++ src/main/java/com/easemob/im/ApiClient.java
@@ -1371,12 +1371,12 @@ public class ApiClient {
     public RequestBody serialize(Object obj, String contentType) throws ApiException {
         if (obj instanceof byte[]) {
             // Binary (byte array) body parameter support.
-            return RequestBody.create(MediaType.parse(contentType), (byte[]) obj);
+            return RequestBody.create((byte[]) obj, MediaType.parse(contentType));
         } else if (obj instanceof File) {
             // File body parameter support.
-            return RequestBody.create(MediaType.parse(contentType), (File) obj);
+            return RequestBody.create((File) obj, MediaType.parse(contentType));
         } else if ("text/plain".equals(contentType) && obj instanceof String) {
-            return RequestBody.create(MediaType.parse(contentType), (String) obj);
+            return RequestBody.create((String) obj, MediaType.parse(contentType));
         } else if (isJsonMime(contentType)) {
             String content;
             if (obj != null) {
@@ -1384,9 +1384,9 @@ public class ApiClient {
             } else {
                 content = null;
             }
-            return RequestBody.create(MediaType.parse(contentType), content);
+            return RequestBody.create(content, MediaType.parse(contentType));
         } else if (obj instanceof String) {
-            return RequestBody.create(MediaType.parse(contentType), (String) obj);
+            return RequestBody.create((String) obj, MediaType.parse(contentType));
         } else {
             throw new ApiException("Content type \"" + contentType + "\" is not supported");
         }
@@ -1637,7 +1637,7 @@ public class ApiClient {
                 reqBody = null;
             } else {
                 // use an empty request body (for POST, PUT and PATCH)
-                reqBody = RequestBody.create(contentType == null ? null : MediaType.parse(contentType), "");
+                reqBody = RequestBody.create("", contentType == null ? null : MediaType.parse(contentType));
             }
         } else {
             reqBody = serialize(body, contentType);
@@ -1711,7 +1711,7 @@ public class ApiClient {
                 reqBody = null;
             } else {
                 // use an empty request body (for POST, PUT and PATCH)
-                reqBody = RequestBody.create(contentType == null ? null : MediaType.parse(contentType), "");
+                reqBody = RequestBody.create("", contentType == null ? null : MediaType.parse(contentType));
             }
         } else {
             reqBody = serialize(body, contentType);
@@ -1930,7 +1930,7 @@ public class ApiClient {
     private void addPartToMultiPartBuilder(MultipartBody.Builder mpBuilder, String key, File file) {
         Headers partHeaders = Headers.of("Content-Disposition", "form-data; name=\"" + key + "\"; filename=\"" + file.getName() + "\"");
         MediaType mediaType = MediaType.parse(guessContentTypeFromFile(file));
-        mpBuilder.addPart(partHeaders, RequestBody.create(mediaType, file));
+        mpBuilder.addPart(partHeaders, RequestBody.create(file, mediaType));
     }
 
     /**
@@ -1943,7 +1943,7 @@ public class ApiClient {
     private void addPartToMultiPartBuilder(MultipartBody.Builder mpBuilder, String key, Object obj) {
         RequestBody requestBody;
         if (obj instanceof String) {
-            requestBody = RequestBody.create(MediaType.parse("text/plain"), (String) obj);
+            requestBody = RequestBody.create((String) obj, MediaType.parse("text/plain"));
         } else {
             String content;
             if (obj != null) {
@@ -1951,7 +1951,7 @@ public class ApiClient {
             } else {
                 content = null;
             }
-            requestBody = RequestBody.create(MediaType.parse("application/json"), content);
+            requestBody = RequestBody.create(content, MediaType.parse("application/json"));
         }
 
         Headers partHeaders = Headers.of("Content-Disposition", "form-data; name=\"" + key + "\"");
