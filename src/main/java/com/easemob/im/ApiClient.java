e144849fc328f6e723bed03ea076b3ab7de0e893
傅东海 <296322762@qq.com>
2025-03-03 21:49:18 +0800

    修改sdk版本号

--- src/main/java/com/easemob/im/ApiClient.java
+++ src/main/java/com/easemob/im/ApiClient.java
@@ -78,7 +78,7 @@ public class ApiClient {
 
     private String appCert;
 
-    public static String version = "1.0.6";
+    public static String version = "1.0.12";
 
     public static Builder builder() {
         return new Builder().setRealm(Realm.EASEMOB_REALM);
